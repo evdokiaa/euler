7f9d64579c39941eabddf8fc6132938dcb569bd2
evdokia.barouxi <ebarouxi@gmail.com>
2017-12-31 11:27:30 +0200

    corrections

---
+++ pythagorean.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+  for (int i = 1; i < 1000; i++) {
+      for (int j = i; j < 1000 - i; j++ ) {
+          if ( i + j + sqrt (i * i + j * j) == 1000 ) 
+              cout << i << " " << j ;
+      }
+   }
+}
